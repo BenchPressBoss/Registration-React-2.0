4ed4c24226474705d3d282526dbbd47819a24523
Maxim Sychev <work2882@outlook.com>
2024-10-30 15:56:15 -0700

    React registration

---
+++ back/prisma/migrations/20241030214304_initial_setup/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'PREMIUM', 'MANAGER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "avatarPath" TEXT,
+    "verification_token" TEXT,
+    "rights" "Role"[] DEFAULT ARRAY['USER']::"Role"[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
